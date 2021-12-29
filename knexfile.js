1e210f0034c6e414979c90d4943632444fbf0954
ferhatosanoglu <ferhatosanogluo@gmail.com>
2021-12-29 13:14:38 +0300

    fixed: solve ssl error

--- knexfile.js
+++ knexfile.js
@@ -15,7 +15,11 @@ module.exports = {
     },
     production: {
         client: 'pg',
-        connection: process.env.DATABASE_URL,
+        connection: {
+            connectionString: process.env.DATABASE_URL,
+            ssl: { rejectUnauthorized: false }
+        },
+
         migrations: {
             directory: './database'
         },
